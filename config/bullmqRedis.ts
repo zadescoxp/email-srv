c6d7c4ffe05f44b17d001f30b02ed6bdb800739c
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-04-17 21:08:02 +0530

    sex

--- config/bullmqRedis.ts
+++ config/bullmqRedis.ts
@@ -1,6 +1,5 @@
 import IORedis from 'ioredis';
 
-// Create an IORedis connection for BullMQ
 export const bullmqRedis = new IORedis(process.env.REDIS_URL as string, {
   password: process.env.REDIS_TOKEN,
   maxRetriesPerRequest: 5,
